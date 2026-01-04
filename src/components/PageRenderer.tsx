f6c00b6d23f3b9e36e8b5f8f742484d74892f237
Omar Zaher <omar.zaher@signify.com>
2026-01-04 19:52:37 +0200

    Remove non-existent TextBlock component

--- src/components/PageRenderer.tsx
+++ src/components/PageRenderer.tsx
@@ -1,13 +1,10 @@
 import Hero from "./Hero";
-import TextBlock from "./TextBlock";
 
 export default function PageRenderer({ sections }: { sections: any[] }) {
   return sections.map((section, i) => {
     switch (section._type) {
       case "hero":
         return <Hero key={i} {...section} />;
-      case "textBlock":
-        return <TextBlock key={i} {...section} />;
       default:
         return null;
     }

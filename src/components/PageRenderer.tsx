bbd6c49fcbe8f5a87a57fb1bf7c0c927eac2be82
Omar Zaher <omar.zaher@signify.com>
2026-01-04 19:57:30 +0200

    Fix Hero component import

--- src/components/PageRenderer.tsx
+++ src/components/PageRenderer.tsx
@@ -1,10 +1,7 @@
-import Hero from "./Hero";
 
 export default function PageRenderer({ sections }: { sections: any[] }) {
   return sections.map((section, i) => {
     switch (section._type) {
-      case "hero":
-        return <Hero key={i} {...section} />;
       default:
         return null;
     }

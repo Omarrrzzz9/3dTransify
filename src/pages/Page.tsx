7592ef332c81bd9eb402ebe7c930f6ec67e0d0ac
Omar Zaher <omar.zaher@signify.com>
2026-01-04 19:43:47 +0200

    Fix import path for getPage

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getPage } from "@/lib/getPage";
-import PageRenderer from "@/components/PageRenderer";
+import { getPage } from "../lib/getPage";
+import PageRenderer from "../components/PageRenderer";
 
 export default function Page({ slug: fixedSlug }: { slug?: string }) {
   const params = useParams();
